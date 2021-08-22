a5bffe537c0a2ac7b1194fd8a6c7090798e163d9
soravitpuengsema <s6301012630184@email.kmutnb.ac.th>
2021-08-22 23:28:00 +0700

    add save/load progress XML

--- board.java
+++ board.java
@@ -1,7 +1,21 @@
-import javax.swing.*;
+package Sorting_game_GUI_java;
+
+import java.util.*;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import java.io.File;
+
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
+
+import javax.swing.*;
 
 class drawing extends Canvas{
     public void paint(Graphics g) {
@@ -34,6 +48,7 @@ public class board implements MouseListener{
     public JFrame f;
     public static void main(String[] args) {
         random_board();
+        loadxml();
         board j = new board();
         j.setup();
     }
@@ -104,9 +119,10 @@ public class board implements MouseListener{
         f.add(l_0);f.add(l_1);f.add(l_2);f.add(l_3);
         f.add(l_4);f.add(l_5);f.add(l_6);f.add(l_7);
         f.add(l_8);f.add(l_9);f.add(l_10);f.add(l_11);
-        f.add(d);
 
         f.addMouseListener(this);
+
+        f.add(d);
         f.setSize(510,535);
         f.setVisible(true);
         f.setLayout(null);
@@ -150,6 +166,7 @@ public class board implements MouseListener{
                 board[mX][mY] = board[mX][mY-1];
                 board[mX][mY-1] = temp;
             }
+            savexml();
             if(check_winner() == true){
                 f.setVisible(false);
                 JFrame winner = new JFrame("Sorting_Game");
@@ -215,4 +232,73 @@ public class board implements MouseListener{
             return false;
         }
     }
+    public static void savexml() {
+        try{
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.newDocument();
+
+            Element sorting_game = doc.createElement("sorting_game");
+            doc.appendChild(sorting_game);
+
+            Element map = doc.createElement("Map");
+            sorting_game.appendChild(map);
+
+            Element row1 = doc.createElement("row1");
+            row1.appendChild(doc.createTextNode(board[0][0]+board[0][1]+board[0][2]+board[0][3]));
+            map.appendChild(row1);
+
+            Element row2 = doc.createElement("row2");
+            row2.appendChild(doc.createTextNode(board[1][0]+board[1][1]+board[1][2]+board[1][3]));
+            map.appendChild(row2);
+
+            Element row3 = doc.createElement("row3");
+            row3.appendChild(doc.createTextNode(board[2][0]+board[2][1]+board[2][2]+board[2][3]));
+            map.appendChild(row3);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("save.xml"));
+            transformer.transform(source, result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadxml(){
+        try{
+
+            File load = new File("save.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(load);
+
+            String row = "";
+
+            for ( int i = 1 ; i < 4 ; i++ ) {
+                row = row + doc.getElementsByTagName("row"+ String.valueOf(i)).item(0).getTextContent();
+            }
+
+            String rowsplit[] = row.split("");
+
+            int s = 0;
+            for (int i = 0; i < 3; i++){
+                for (int j = 0; j < 4; j++){
+                    board[i][j] = rowsplit[s];
+                    s += 1;
+                }
+            }
+            if(Arrays.deepEquals(board,sorted_board)){
+                random_board();
+                savexml();
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
+
 }
